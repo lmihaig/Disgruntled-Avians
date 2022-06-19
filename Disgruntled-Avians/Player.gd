67f63cfae298a4f3116da85018a6806f8f2f5a48
Licu Mihai <licu.mihai21@gmail.com>
2022-06-20 00:08:56 +0300

    basic player

---
+++ Disgruntled-Avians/Player.gd
@@ -0,0 +1,26 @@
+extends Area2D
+
+export var speed = 400.0
+var screen_size = Vector2.ZERO
+
+func _ready():
+	screen_size = get_viewport_rect().size
+
+func _process(delta):
+	var direction = Vector2.ZERO
+	if Input.is_action_just_pressed("move_right"):
+		direction.x += 1
+	if Input.is_action_just_pressed("move_left"):
+		direction.x -= 1
+	if Input.is_action_just_pressed("move_up"):
+		direction.y -= 1
+	if Input.is_action_just_pressed("move_down"):
+		direction.y += 1
+
+	if direction.length() > 1:
+		direction = direction.normalized()
+		
+	position += direction * speed * delta
+	position.x = clamp(position.x, 0, screen_size.x)
+	position.y = clamp(position.y, 0, screen_size.y)
+	
